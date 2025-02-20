099c85aa0bc0e2a20d17c4579bf47a418f540170
sirisk <adamchrisk@gmail.com>
2025-02-20 16:25:53 +0200

    integrate drfsimplejwt add login signup endpoints views

--- UserServices/urls.py
+++ UserServices/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from UserServices.Controller import AuthController
 
+
 urlpatterns = [
     path("login/", AuthController.LoginAPIView.as_view(), name="login"),
+    path("signup/", AuthController.SignupAPIView.as_view(), name="login"),
+    path("publicApi/", AuthController.PublicAPIView.as_view(), name="public"),
+    path("protectedApi/", AuthController.ProtectedAPIView.as_view(), name="protected"),
 ]
